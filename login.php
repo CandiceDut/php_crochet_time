82044a4163f2bd263adca397f2ce7611d0456c31
Candice Dutournier <cdutourni001@iutbayonne.univ-pau.fr>
2024-10-08 10:43:17 +0200

    ajout des fichiers accueil.php et identification.html avec modifpour se connecter

--- login.php
+++ login.php
@@ -1,23 +1,21 @@
 <?php
     // On définit un login et un mot de passe de base
-    $login_valide = "moi";
-    $pwd_valide = "lemien";
+    $login_valide = "root";
+    $pwd_valide = "rooteur";
 
     // on teste si nos variables sont définies
     if (isset($_POST['login']) && isset($_POST['pwd'])) {
         // on vérifie les informations saisies
         if ($login_valide == $_POST['login'] && $pwd_valide == $_POST['pwd']) {
             session_start ();
-            // on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd) (
+            // on enregistre les paramètres de notre visiteur comme variables de session ($login et $pwd)
             $_SESSION['login'] = $_POST['login'];
             $_SESSION['pwd'] = $_POST['pwd'];
-            // on redirige notre visiteur vers une page de notre section membre
-            header ('location: page_membre.php');
+            header ('location: accueil.php');
         }
         else {
             echo '<body onLoad="alert(\'Tu fais partie des membres toi ?\')">';
-            // puis on le redirige vers la page d'accueil
-            echo '<meta http-equiv="refresh" content="0;URL=index.htm">';
+            echo '<meta http-equiv="refresh" content="0;URL=identification.html">';
         }
     }
     else {
